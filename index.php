ca54b0b04239903ada2cfa6a62a9a524cb5c8959
loekes <lukas.raymaekers@student.ehb.be>
2016-10-24 11:27:16 +0200

    Finished form

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width; initial-scale = 1.0; maximum-scale=1.0; user-scalable=no" />
+        <meta name="theme-color" content="#647187">
         <title>WebApp</title>
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
@@ -47,6 +48,9 @@
                     </div>
                     <div class="row spaced">
                         <div class="col-md-6"></div>
+                        <div class="col-md-6">
+                            <span class="pull-right"><a class="btn btn-primary btn-lg" href="#" role="button">Search</a></span>
+                        </div>
                     </div>
                  </div>
             </div>
