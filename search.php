d383cc805d2c282f5f75cba4b7e6aaca3ac6fb30
loekes <lukas.raymaekers@student.ehb.be>
2016-10-24 11:55:59 +0200

    Add search.php

---
+++ search.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width; initial-scale = 1.0; maximum-scale=1.0; user-scalable=no" />
+        <meta name="theme-color" content="#647187">
+        <title>WebApp</title>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+        <link rel="stylesheet" href="resources/css/main.css">
+    </head>
+    <body>
+        <div id="search" class="container-fluid">
+            <div class="container">
+                <h1>WebApp</h1>
+                <div class="row">
+                    <div class="col-md-2"><h4>IC 1234</h4></div>
+                    <div class="col-md-8"><h4>Antwerpen-Centraal - Brussel-Zuid</h4></div>
+                    <div class="col-md-2"><h4>12:15 - Spoor 24</h4></div>
+                </div>
+                <div class="row">
+                </div> 
+                <div class="row">
+                    <div class="col-md-2"><h4>IC 1234</h4></div>
+                    <div class="col-md-8"><h4>Antwerpen-Centraal - Brussel-Zuid</h4></div>
+                    <div class="col-md-2"><h4>12:30 - Spoor 24</h4></div>
+                </div>
+                <div class="row">
+                </div> 
+                <div class="row">
+                    <div class="col-md-2"><h4>IC 1234</h4></div>
+                    <div class="col-md-8"><h4>Antwerpen-Centraal - Brussel-Zuid</h4></div>
+                    <div class="col-md-2"><h4>12:45 - Spoor 24</h4></div>
+                </div>
+                <div class="row">
+                </div> 
+            </div>
+        </div>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+    </body>
+</html>
