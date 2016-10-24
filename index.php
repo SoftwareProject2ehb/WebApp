2c6504af6ac6bed7b109a112641ff7bce285f60f
loekes <lukas.raymaekers@student.ehb.be>
2016-10-24 10:28:42 +0200

    Add search container and style

--- index.php
+++ index.php
@@ -9,7 +9,11 @@
         <link rel="stylesheet" href="resources/css/main.css">
     </head>
     <body>
-        <!-- body comes here -->
+        <div id="search" class="container-fluid">
+            <div class="container">
+                <h1>WebApp</h1>
+            </div>
+        </div>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
     </body>
