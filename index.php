5b67e549923594d018206e2e6c1e479cce69d73c
loekes <lukas.raymaekers@student.ehb.be>
2016-10-25 12:01:38 +0200

    Add menu + button for later and earlier trains

--- index.php
+++ index.php
@@ -10,9 +10,30 @@
         <link rel="stylesheet" href="resources/css/main.css">
     </head>
     <body>
+    <nav class="navbar navbar-default">
+            <div class="container">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="index.php">WebApp</a>
+                </div>
+                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                    <ul class="nav navbar-nav">
+                        <li class="active"><a href="index.php">Search Trains <span class="sr-only">(current)</span></a></li>
+                        <li><a href="tickets/">Tickets</a></li>
+                        <li><a href="About/">About</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
         <div id="search" class="container-fluid">
             <div class="container">
-                <h1>WebApp</h1>
+                <h1>Search Trains</h1>
                 <div class="form-group">
                     <div class="row">
                         <div class="col-md-6 col-sx-6">
@@ -49,7 +70,7 @@
                     <div class="row spaced">
                         <div class="col-md-6"></div>
                         <div class="col-md-6">
-                            <span class="pull-right"><a class="btn btn-primary btn-lg" href="#" role="button">Search</a></span>
+                            <span class="pull-right"><a class="btn btn-primary btn-lg" href="search/" role="button">Search</a></span>
                         </div>
                     </div>
                  </div>
--- search/index.php
+++ search/index.php
@@ -7,14 +7,42 @@
         <title>WebApp</title>
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
-        <link rel="stylesheet" href="resources/css/main.css">
+        <link rel="stylesheet" href="../resources/css/main.css">
     </head>
     <body>
-        <div id="search" class="container-fluid">
+    <nav class="navbar navbar-default">
             <div class="container">
-                <h1>WebApp</h1>
-                
-               <div class="row">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="../index.php">WebApp</a>
+                </div>
+                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                    <ul class="nav navbar-nav">
+                        <li class="active"><a href="#">Search Results <span class="sr-only">(current)</span></a></li>
+                        <li><a href="../">New Search</a></li>
+                        <li><a href="../tickets/">Tickets</a></li>
+                        <li><a href="../About/">About</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
+
+        <div id="search-results" class="container-fluid">
+            <div class="container">
+            <h1>Search Results</h1>
+            <div class="row">
+                <div class="col-md-12 col-xs-12">
+                    <button type="button" class="btn btn-info">Earlier Trains</button>
+                    <span class="pull-right"><button type="button" class="btn btn-info">Later Trains</button></span>
+                </div>
+            </div>
+            <div class="row spaced">
                     <div class="col-md-2 col-xs-5">
                         <h4><span class="glyphicon glyphicon-time status delayed"></span> IC 1234</h4>
                     </div>
@@ -185,7 +213,13 @@
                         </div>
                     </div>
                 </div>
-
+                    <div class="row spaced">
+                    <div class="col-md-12 col-xs-12">
+                        <button type="button" class="btn btn-info">Earlier Trains</button>
+                        <span class="pull-right"><button type="button" class="btn btn-info">Later Trains</button></span>
+                    </div>
+                
+                </div>
             </div>
         </div>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
